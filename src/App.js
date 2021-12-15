1ff5e4cecf3f45d9024997e897e9bc34bc3f1ad7
AntonioRosales1 <elrluis213@gmail.com>
2021-12-14 22:29:33 -0800

    Adding first function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {increment} from './actions';
 
-
-function App() {
+ 
+function App(){
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
     <h1>My App</h1>
-    <h2>I love programming</h2>
+    <h2>counter {counter}</h2>
+    <button onClick={() => dispatch(increment())}>+</button>
+    <button>-</button>
+
+        {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ''}
     </div>
   );
 }
